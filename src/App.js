1005751c52bec1221710351375fb425982b876b1
Dilshan <dilshansamarakoon123@gmail.com>
2023-07-05 12:49:11 +0530

    Develop a gym website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Hero } from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Reasons from './Components/Reasons/Reasons';
+
+function App() {
+  return (
+    <div className="App">
+       <Hero/>
+       <Programs/>
+       <Reasons/>   
+    </div>
+  );
+}
+
+export default App;
